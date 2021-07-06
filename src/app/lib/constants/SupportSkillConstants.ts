390a6ad1657c252fa64ad80bcde31414bae37c26
Joshua Hornsby <joshuahornsby89@gmail.com>
2021-07-05 21:57:30 -0400

    Added Support Skill Constants

--- src/app/lib/constants/SupportSkillConstants.ts
+++ src/app/lib/constants/SupportSkillConstants.ts
@@ -10,12 +10,17 @@ import { SupportSkill } from './../SupportSkill';
 
 export const adroitWisdom: SupportSkill = {
 	name: 'Adroit Wisdom',
-	description: 'Increases INT by 15%'
+	description: 'Increases INT by 15%.'
 };
 
 export const allRounder: SupportSkill = {
 	name: 'All-Rounder',
-	description: 'Increases ATK, DEF, INT, and SPD by 5%.'
+	description: 'Increases ATK, DEF, INT and SPD by 5%.'
+};
+
+export const analyze: SupportSkill = {
+	name: 'Analyze',
+	description: 'Increases scan values by 10%.'
 };
 
 export const animalColosseum: SupportSkill = {
@@ -23,9 +28,34 @@ export const animalColosseum: SupportSkill = {
 	description: 'Increases damage from Earth skills by 15%.'
 };
 
+export const ausGenerics: SupportSkill = {
+	name: 'Aus Generics',
+	description: 'Increases SPD and EVA by 25% when HP drops below 25%.'
+};
+
+export const backwaterCamp: SupportSkill = {
+	name: 'Backwater Camp',
+	description: 'Increases damage given by 20%, but also increases damage received by 20%.'
+};
+
 export const battleSense: SupportSkill = {
 	name: 'Battle Sense',
-	description: 'A tiny dragon Digimon with a cool white sheen. Unfettered and liking it that way, it lives for adventure, likely due to Gankoomon\'s DNA. It calmly takes on Gankoomon\'s rigorous trials. Its lineage affords it a heightened sense of combat, and its training with the Sistermon sisters puts it on par with Ultimate opponents in a fight. It specializes in quick and agile melee combat and its special move Fifth Rush cuts up its opponent with sturdy claws. Its dream is to become a Royal Knight like Gankoomon.'
+	description: 'Increases ATK by 50% when HP drops below 10%.'
+};
+
+export const beastSpirit: SupportSkill = {
+	name: 'Beast Spirit',
+	description: 'Increases ATK by 20%, but reduces SPD by 10%.'
+};
+
+export const blastDigivolution: SupportSkill = {
+	name: 'Blast Digivolution',
+	description: 'Increases damage given by 10%.'
+};
+
+export const blaster: SupportSkill = {
+	name: 'Blaster',
+	description: 'Increases ATK by 10% and amount of HP received from absorption effects by 30%.'
 };
 
 export const bullsEye: SupportSkill = {
@@ -33,15 +63,50 @@ export const bullsEye: SupportSkill = {
 	description: 'Increases ACU by 15%.'
 };
 
-export const dragonsRoar: SupportSkill = {
-	name: 'Dragon\'s Roar',
-	description: 'Increases damage from Fire skills by 15%.'
+export const caledfwlchDive: SupportSkill = {
+	name: 'Caledfwlch Dive',
+	description: 'Increases ACU and EVA by 5%, and CRT by 10%.'
 };
 
-export const dragonsWrath: SupportSkill = {
-	name: 'Dragon\'s Wrath',
-	description: "Increases ATK by 15%"
-}
+export const chaosExpert: SupportSkill = {
+	name: 'Chaos Expert',
+	description: 'Increases ATK and INT by 10%.'
+};
+
+export const chiryu: SupportSkill = {
+	name: 'Chiryu',
+	description: 'Increases ATK and damage dealt by Earth skills by 10%.'
+};
+
+export const counterAttack: SupportSkill = {
+	name: 'Counter-Attack',
+	description: 'Increases damage dealt to Royal Knights by 50%.'
+};
+
+export const crimsonFormation: SupportSkill = {
+	name: 'Crimson Formation',
+	description: 'Increases damage from Electric and Wind skills by 10%.'
+};
+
+export const crimsonMode: SupportSkill = {
+	name: 'Crimson Mode',
+	description: 'Increases ATK, DEF, INT, and SPD by 10%.'
+};
+
+export const crossHeart: SupportSkill = {
+	name: 'Cross Heart',
+	description: 'Increases ATK and probability of combos occurring by 8%.'
+};
+
+export const dBrigade: SupportSkill = {
+	name: 'D-Brigade',
+	description: 'Increases ACU and CRT by 10%.'
+};
+
+export const darkLgtThunderCannon: SupportSkill = {
+	name: 'Dark-Lgt, Thunder-Cannon',
+	description: 'Increases damage dealt by Electric, Water, Light, and Dark skills by 8%.'
+};
 
 export const deepSavers: SupportSkill = {
 	name: 'Deep Savers',
@@ -53,14 +118,59 @@ export const deepSeaSlumber: SupportSkill = {
 	description: '10% chance of putting target to sleep when attacking.'
 };
 
+export const destiny: SupportSkill = {
+	name: 'Destiny',
+	description: 'Restores 3% of Max HP and SP every turn.'
+};
+
 export const destroyer: SupportSkill = {
 	name: 'Destroyer',
 	description: '10% chance of instantly killing target when attacking.'
 };
 
+export const digitalHazard: SupportSkill = {
+	name: 'Digital Hazard',
+	description: 'Increases both ATK and damage dealt by Fire and Dark skills by 10%.'
+};
+
+export const dragonMode: SupportSkill = {
+	name: 'Dragon Mode',
+	description: 'Increases DEF and INT by 10%.'
+};
+
+export const dragonsRoar: SupportSkill = {
+	name: 'Dragon\'s Roar',
+	description: 'Increases damage from Fire skills by 15%.'
+};
+
+export const dragonsWrath: SupportSkill = {
+	name: 'Dragon\'s Wrath',
+	description: 'Increases ATK by 15%.'
+};
+
+export const envy: SupportSkill = {
+	name: 'Envy',
+	description: 'Increases damage done to targets by 15% if they are Mega or above.'
+};
+
+export const evilHeart: SupportSkill = {
+	name: 'Evil Heart',
+	description: 'Increases amount of SP received from absorption effects by 30%.'
+};
+
+export const fairyProtection: SupportSkill = {
+	name: 'Fairy Protection',
+	description: 'Increases given stat boost effects by 5%.'
+};
+
 export const feint: SupportSkill = {
-	name: 'feint',
-	description: 'Increases EVA by 7%'
+	name: 'Feint',
+	description: 'Increases EVA by 7%.'
+};
+
+export const feralPower: SupportSkill = {
+	name: 'Feral Power',
+	description: 'Reduces damage received by 10%.'
 };
 
 export const feralPulse: SupportSkill = {
@@ -68,9 +178,24 @@ export const feralPulse: SupportSkill = {
 	description: 'Increases SPD by 15%.'
 };
 
+export const fightOrFlight: SupportSkill = {
+	name: 'Fight or Flight',
+	description: 'Increases ATK by 20% when HP drops below 50%.'
+};
+
+export const fighterMode: SupportSkill = {
+	name: 'Fighter Mode',
+	description: 'Increases ATK and SPD by 10%.'
+};
+
 export const flowerGuard: SupportSkill = {
 	name: 'Flower Guard',
 	description: 'Prevents poison.'
+};
+
+export const freyr: SupportSkill = {
+	name: 'Freyr',
+	description: 'Increases damage from Wind and Light skills by 10%.'
 };
 
 export const fullFunctionality: SupportSkill = {
@@ -78,9 +203,34 @@ export const fullFunctionality: SupportSkill = {
 	description: 'Prevents sleep.'
 };
 
+export const furiousHowl: SupportSkill = {
+	name: 'Furious Howl',
+	description: '30% chance of instantly killing target when attacking if lower than Ultimate.'
+};
+
+export const gehenna: SupportSkill = {
+	name: 'Gehenna',
+	description: 'Reduces damage received from magic attacks by 10% and converts it to HP.'
+};
+
 export const gekirin: SupportSkill = {
 	name: 'Gekirin',
 	description: 'Has a 5% chance to raise ATK and lower DEF by 50% when damage is received.'
+};
+
+export const gluttony: SupportSkill = {
+	name: 'Gluttony',
+	description: 'Reduces HP by 5% of Max HP and increases ATK by 5% every turn.'
+};
+
+export const godOfWar: SupportSkill = {
+	name: 'God of War',
+	description: 'Increases ATK and INT by 10%, and increases damage done by Light skills by 10%.'
+};
+
+export const greed: SupportSkill = {
+	name: 'Greed',
+	description: 'Increases amount of HP and SP received from absorption effects by 20%.'
 };
 
 export const haymaker: SupportSkill = {
@@ -88,9 +238,44 @@ export const haymaker: SupportSkill = {
 	description: 'Increases CRT by 15%.'
 };
 
+export const hermiticLoner: SupportSkill = {
+	name: 'Hermitic Loner',
+	description: 'Increases damage to Royal Knights by 50%.'
+};
+
+export const highResolution: SupportSkill = {
+	name: 'High Resolution',
+	description: 'Prevents dotting.'
+};
+
+export const holyKnight: SupportSkill = {
+	name: 'Holy Knight',
+	description: 'Increases SPD by 10%, and damage from Fire and Light skills by 10%.'
+};
+
 export const holyRing: SupportSkill = {
 	name: 'Holy Ring',
 	description: 'Increases HP recovery effects by 20%.'
+};
+
+export const holySalvation: SupportSkill = {
+	name: 'Holy Salvation',
+	description: 'Reduces damage received from species with bad affinity by 15%.'
+};
+
+export const humanSpirit: SupportSkill = {
+	name: 'Human Spirit',
+	description: 'Increases SPD by 20%, but reduces ATK by 10%.'
+};
+
+export const immobility: SupportSkill = {
+	name: 'Immobility',
+	description: 'Prevents stunning.'
+};
+
+export const infinityDream: SupportSkill = {
+	name: 'Infinity Dream',
+	description: 'Cuts the rate of abnormal statuses received by 10%, and recovers 5% of HP per turn.'
 };
 
 export const innocentEyes: SupportSkill = {
@@ -103,14 +288,44 @@ export const ironcladDefense: SupportSkill = {
 	description: 'Increases DEF by 15%.'
 };
 
+export const ironcladFortification: SupportSkill = {
+	name: 'Ironclad Fortification',
+	description: 'Reduces damage received by 10%.'
+};
+
 export const justiceGenome: SupportSkill = {
 	name: 'Justice Genome',
 	description: 'Increases damage from Light skills by 15%.'
 };
 
+export const justiceUnion: SupportSkill = {
+	name: 'Justice Union',
+	description: 'Increases probability of combos occurring and continuing by 10%.'
+};
+
 export const legendArms: SupportSkill = {
 	name: 'Legend-Arms',
-	description: 'Increases damage dealtby Light and Dark skills by 10%.'
+	description: 'Increases damage dealt by Light and Dark skills by 10%.'
+};
+
+export const leopardMode: SupportSkill = {
+	name: 'Leopard Mode',
+	description: 'Increases EVA and CRT by 5%, and SPD by 10%.'
+};
+
+export const lust: SupportSkill = {
+	name: 'Lust',
+	description: 'Reduces damage received from physical attacks by 10% and converts it to SP.'
+};
+
+export const masterSDisposition: SupportSkill = {
+	name: 'Master\'s Disposition',
+	description: 'If HP is over 50% of Max HP, a fatal attack will instead leave you with 1 HP.'
+};
+
+export const meditation: SupportSkill = {
+	name: 'Meditation',
+	description: 'Restores 6% of Max HP every turn.'
 };
 
 export const menace: SupportSkill = {
@@ -123,9 +338,29 @@ export const metalEmpire: SupportSkill = {
 	description: 'Increases damage from Electric skills by 15%.'
 };
 
+export const millionaire: SupportSkill = {
+	name: 'Millionaire',
+	description: 'Increases YEN earned in battle.'
+};
+
+export const miracle: SupportSkill = {
+	name: 'Miracle',
+	description: 'Has a 10% chance of reducing damage received by 75%.'
+};
+
+export const mirageBurst: SupportSkill = {
+	name: 'Mirage Burst',
+	description: 'Increases physical damage received by 20%, but also increases INT by 13%.'
+};
+
 export const moonKissedBlade: SupportSkill = {
 	name: 'Moon-Kissed Blade',
 	description: 'Increases damage from Water and Light skills by 10%.'
+};
+
+export const muscleMonkeySuit: SupportSkill = {
+	name: 'Muscle Monkey Suit',
+	description: 'Reduces physical damage received by 15%.'
 };
 
 export const mysteriousScale: SupportSkill = {
@@ -153,9 +388,39 @@ export const numbingWaves: SupportSkill = {
 	description: 'Prevents paralysis.'
 };
 
+export const nxedgeStyle: SupportSkill = {
+	name: 'NXEdge Style',
+	description: 'Raises CRT and probability of combos by 10%.'
+};
+
+export const obsidianKnight: SupportSkill = {
+	name: 'Obsidian Knight',
+	description: 'Increases DEF by 10%, and damage from Water and Dark skills by 10%.'
+};
+
+export const oceanLove: SupportSkill = {
+	name: 'Ocean Love',
+	description: 'Restores 5% of Max SP every turn.'
+};
+
+export const parallelWorldTactician: SupportSkill = {
+	name: 'Parallel World Tactician',
+	description: 'Increases Light and Dark skill damage by 10% and increases HP recovery effects by 10%.'
+};
+
 export const phoenixFeathers: SupportSkill = {
 	name: 'Phoenix Feathers',
 	description: 'Prevents instant death.'
+};
+
+export const platinumBonus: SupportSkill = {
+	name: 'Platinum Bonus',
+	description: 'Significantly increases EXP earned in battle.'
+};
+
+export const platinumService: SupportSkill = {
+	name: 'Platinum Service',
+	description: 'Increases EXP earned in battle.'
 };
 
 export const poisonThorns: SupportSkill = {
@@ -163,14 +428,109 @@ export const poisonThorns: SupportSkill = {
 	description: '10% chance of poisoning target when attacking.'
 };
 
+export const pride: SupportSkill = {
+	name: 'Pride',
+	description: 'Increases damage from all attribute skills by 5%.'
+};
+
+export const primalTrooper: SupportSkill = {
+	name: 'Primal Trooper',
+	description: 'Increases damage from Neutral skills by 15%.'
+};
+
+export const purgeShine: SupportSkill = {
+	name: 'Purge Shine',
+	description: 'Reduces damage taken from attacks from bad matchups by 10% and converts it to SP.'
+};
+
+export const revelation: SupportSkill = {
+	name: 'Revelation',
+	description: 'Increases damage from Neutral skills by 20%, but decreases all other skill damage by 10%.'
+};
+
 export const royalCommando: SupportSkill = {
 	name: 'Royal Commando',
 	description: '10% chance of paralyzing target when attacking.'
 };
 
+export const samuraiSpirit: SupportSkill = {
+	name: 'Samurai Spirit',
+	description: 'Increases ATK and CRT by 10%.'
+};
+
+export const scavenge: SupportSkill = {
+	name: 'Scavenge',
+	description: 'Increases foe drop rate for items.'
+};
+
+export const shiningBurst: SupportSkill = {
+	name: 'Shining Burst',
+	description: 'Increases magic damage received by 50%, but also increases ATK by 25%.'
+};
+
+export const sloth: SupportSkill = {
+	name: 'Sloth',
+	description: 'Prevents Sleep, and has a 30% chance of putting target to sleep when attacking.'
+};
+
+export const strangeDance: SupportSkill = {
+	name: 'Strange Dance',
+	description: '10% chance of confusing target when attacking.'
+};
+
+export const strategist: SupportSkill = {
+	name: 'Strategist',
+	description: 'Reduces SP consumed by skills by 20%.'
+};
+
+export const supervisor: SupportSkill = {
+	name: 'Supervisor',
+	description: 'Slightly increases EXP from battles, and reduces damage taken by 5%.'
+};
+
+export const supremeFocus: SupportSkill = {
+	name: 'Supreme Focus',
+	description: 'Prevents confusion.'
+};
+
+export const swiftness: SupportSkill = {
+	name: 'Swiftness',
+	description: 'Makes sure you always move first in battle. (Only affects the caster.)'
+};
+
+export const tenryu: SupportSkill = {
+	name: 'Tenryu',
+	description: 'Increases SPD and damage dealt by Wind skills by 10%.'
+};
+
+export const tifaret: SupportSkill = {
+	name: 'Tifaret',
+	description: 'Restores 5% of max HP every turn, and increases HP received from absorption effects by 20%.'
+};
+
 export const toFightAnotherDay: SupportSkill = {
 	name: 'To Fight Another Day',
 	description: 'Ensures successful escape from any battle you can escape from.'
+};
+
+export const trickster: SupportSkill = {
+	name: 'Trickster',
+	description: '10% chance of dotting target when attacking.'
+};
+
+export const unmatchedCruelty: SupportSkill = {
+	name: 'Unmatched Cruelty',
+	description: 'Increases damage from attributes with good affinity by 15%.'
+};
+
+export const unstableSingularity: SupportSkill = {
+	name: 'Unstable Singularity',
+	description: 'Increases damage given by 30%, but also increases damage received by 30%.'
+};
+
+export const unwaveringDefense: SupportSkill = {
+	name: 'Unwavering Defense',
+	description: 'Greatly decreases EVA, but also reduces damage received by 15%.'
 };
 
 export const utopia: SupportSkill = {
@@ -183,7 +543,27 @@ export const vampire: SupportSkill = {
 	description: 'Increases HP received from absorption effects by 25%.'
 };
 
+export const vampireKing: SupportSkill = {
+	name: 'Vampire King',
+	description: 'Increases HP from absorption effects by 25%, and damage dealt by Dark skills by 10%.'
+};
+
 export const windGuardians: SupportSkill = {
 	name: 'Wind Guardians',
 	description: 'Increases damage from Wind skills by 15%.'
+};
+
+export const windFlameIceSword: SupportSkill = {
+	name: 'Wind-Flame, Ice-Sword',
+	description: 'Increases damage dealt by Fire, Wind, Plant, and Earth skills by 8%.'
+};
+
+export const wrath: SupportSkill = {
+	name: 'Wrath',
+	description: 'Raises INT by 50% when HP drops below 25%.'
+};
+
+export const wyvern: SupportSkill = {
+	name: 'Wyvern',
+	description: 'Increases damage from Fire and Wind skills by 10%.'
 };
